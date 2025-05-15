3629a72083c7597cba2520f49b8efaaa1489af5c
Gijsbert ter Horst <gijsbert.terhorst@intar.nl>
2025-05-15 11:31:05 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+
+var engine = new Jint.Engine();
+engine.SetValue(
+	"log",
+	(string prefix, object? obj) => Console.WriteLine($"{prefix, 40}: {JsonSerializer.Serialize(obj)}")
+);
+engine.SetValue("csEvaluate", (Func<object> f) => f());
+engine.Evaluate(
+	"""
+	const counter = () => { let i = 0; return () => i++; };
+
+  log('csEvaluate counter', csEvaluate(counter()));
+  log('csEvaluate counter again', csEvaluate(counter()));
+"""
+);
